8742908eeeabd740a05f41e20c78de2ca83a8853
Pushparaj Singh <pushparajsingh448@gmail.com>
2022-06-27 18:33:30 +0530

    Updates

---
+++ precache-manifest.98b319f877aae75706c9733244db1700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2579620b3cab03be0c3565d0f2a7444",
+    "url": "/simple-redux-project/index.html"
+  },
+  {
+    "revision": "9906032d5c02e20221b0",
+    "url": "/simple-redux-project/static/css/main.ebd6ce47.chunk.css"
+  },
+  {
+    "revision": "282f7958e5880d283f63",
+    "url": "/simple-redux-project/static/js/2.64ea4e5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-redux-project/static/js/2.64ea4e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9906032d5c02e20221b0",
+    "url": "/simple-redux-project/static/js/main.abde7757.chunk.js"
+  },
+  {
+    "revision": "95476b9d3dd0a45cdb12",
+    "url": "/simple-redux-project/static/js/runtime-main.0d284761.js"
+  }
+]);
